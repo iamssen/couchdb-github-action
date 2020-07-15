a7eeb6168ff608b3547b6cc9fc585e3a750841ee
SSen <i@ssen.name>
2020-07-15 22:26:10 +0900

    test: try fix couchdb wating

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,16 @@ if [ -z "$COUCHDB_VERSION" ]; then
 fi
 
 echo "Starting CouchDB"
-docker run -detach --name couchdb --publish 5984:5984 --env COUCHDB_USER=admin --env COUCHDB_PASSWORD:password couchdb:$COUCHDB_VERSION
+docker run -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d couchdb:$COUCHDB_VERSION
 
 function waiting() {
   echo "Waiting for CouchDB run..."
 
-  while ! curl -f http://localhost:5984 &> /dev/null
+  while ! curl -f http://127.0.0.1:5984 &> /dev/null
   do
-    echo -ne "."
-    sleep 1
+    echo $(curl -o /dev/null -w "%{http_code}" "http://127.0.0.1:5984/")
+    # echo -ne "."
+    sleep 5
   done
 }
 
