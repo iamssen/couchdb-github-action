d1db25de3ec348c4b33768130b7d2bff151e9ed8
SSen <i@ssen.name>
2020-07-15 22:58:34 +0900

    fix: couchdb does not start

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,10 @@ docker run -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d co
 function waiting() {
   echo "Waiting for CouchDB run..."
 
-  while ! curl -f http://127.0.0.1:5984 &> /dev/null
+  while ! curl -f http://localhost:5984 &> /dev/null
   do
-    echo $(curl -o /dev/null -w "%{http_code}" "http://127.0.0.1:5984/")
+    # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:5984")
+    echo $(curl -f http://localhost:5984)
     # echo -ne "."
     sleep 5
   done
