180184c4b39932583fcf4d944838448632f3da50
Seo Yeon, Lee <i@ssen.name>
2020-07-15 23:18:57 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,18 @@ echo "Check dependencies"
 echo "- curl : $(which curl)"
 
 echo "Starting CouchDB"
-docker run -p 5984:19764 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d couchdb:$COUCHDB_VERSION
+docker run -p 5984:5984 -d couchdb:$COUCHDB_VERSION
 
 function waiting() {
   echo "Waiting for CouchDB run..."
 
-  while ! curl -f http://localhost:19764 &> /dev/null
+  while ! curl -f http://localhost:5984 &> /dev/null
   do
     # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:19764")
-    echo $(curl -f http://localhost:19764)
+    echo $(curl -f http://localhost:5984)
+    echo $(curl -f http://0.0.0.0:5984)
+    echo $(curl -f http://127.0.0.1:5984)
+    echo $(curl -f https://127.0.0.1:5984)
     echo $(docker ps)
     # echo -ne "."
     sleep 5
