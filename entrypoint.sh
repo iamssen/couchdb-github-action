1d5035771c103895439c973886cc05f2fbafd2ef
SSen <i@ssen.name>
2020-07-16 11:31:01 +0900

    feat: improve waiting script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 COUCHDB_VERSION=$1
 
-if [ -z "$COUCHDB_VERSION" ]; then
+if [[ -z "$COUCHDB_VERSION" ]]; then
   echo "Missing CouchDB version in the [couchdb-version] input. Received value: $COUCHDB_VERSION"
   exit 2
 fi
@@ -10,27 +10,34 @@ fi
 echo "Environments"
 echo "- curl : $(which curl)"
 
-echo "Starting CouchDB"
-docker run --name my-couchdb-app -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -v "/github/workspace/local.d":"/opt/couchdb/etc/local.d" -d couchdb:$COUCHDB_VERSION
+echo "Starting CouchDB..."
+docker run --name my-couchdb-app -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d couchdb:$COUCHDB_VERSION
 docker ps
-#docker exec my-couchdb-app ls /opt/couchdb/etc/local.d
 
-# FIXME can't connect. reason is unknown.
-sleep 20
-# function waiting() {
-#   echo "Waiting for CouchDB run..."
+function waiting() {
+  echo "Waiting for CouchDB run..."
 
-#   while ! curl -f http://localhost:5984 &> /dev/null
-#   do
-#     # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:19764")
-#     echo $(curl -f http://localhost:5984)
-#     echo $(curl -f http://0.0.0.0:5984)
-#     echo $(curl -f http://127.0.0.1:5984)
-#     echo $(curl -f https://127.0.0.1:5984)
-#     echo $(docker ps)
-#     # echo -ne "."
-#     sleep 5
-#   done
-# }
+  TIMEOUT=20
+  TIMER=$TIMEOUT
 
-# waiting
+  while ! curl -f http://127.0.0.1:5984 &> /dev/null
+  do
+    sleep 1
+    # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:19764")
+    echo $(curl -f http://localhost:5984)
+    echo $(curl -f http://0.0.0.0:5984)
+    echo $(curl -f http://127.0.0.1:5984)
+    echo $(curl -f https://127.0.0.1:5984)
+    echo $(docker ps)
+    # echo -ne "."
+
+    TIMER=$((TIMER - 1))
+
+    if [[ $TIMER -eq 0 ]]; then
+      echo "CouchDB did not run within $TIMEOUT seconds."
+      exit 2
+    fi
+  done
+}
+
+waiting
