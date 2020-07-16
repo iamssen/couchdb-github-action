af9b028b60e1b898b4ab6eee51ac42c5230bee65
SSen <i@ssen.name>
2020-07-16 11:34:23 +0900

    fix: rollback waiting script

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,30 +14,33 @@ echo "Starting CouchDB..."
 docker run --name my-couchdb-app -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d couchdb:$COUCHDB_VERSION
 docker ps
 
-function waiting() {
-  echo "Waiting for CouchDB run..."
+# FIXME can't connect. reason is unknown.
+sleep 15
 
-  TIMEOUT=20
-  TIMER=$TIMEOUT
-
-  while ! curl -f http://127.0.0.1:5984 &> /dev/null
-  do
-    sleep 1
-    # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:19764")
-    echo $(curl -f http://localhost:5984)
-    echo $(curl -f http://0.0.0.0:5984)
-    echo $(curl -f http://127.0.0.1:5984)
-    echo $(curl -f https://127.0.0.1:5984)
-    echo $(docker ps)
-    # echo -ne "."
-
-    TIMER=$((TIMER - 1))
-
-    if [[ $TIMER -eq 0 ]]; then
-      echo "CouchDB did not run within $TIMEOUT seconds."
-      exit 2
-    fi
-  done
-}
-
-waiting
+#function waiting() {
+#  echo "Waiting for CouchDB run..."
+#
+#  TIMEOUT=20
+#  TIMER=$TIMEOUT
+#
+#  while ! curl -f http://127.0.0.1:5984 &> /dev/null
+#  do
+#    sleep 1
+#    # echo $(curl -o /dev/null -w "%{http_code}" "http://localhost:19764")
+#    echo $(curl -f http://localhost:5984)
+#    echo $(curl -f http://0.0.0.0:5984)
+#    echo $(curl -f http://127.0.0.1:5984)
+#    echo $(curl -f https://127.0.0.1:5984)
+#    echo $(docker ps)
+#    # echo -ne "."
+#
+#    TIMER=$((TIMER - 1))
+#
+#    if [[ $TIMER -eq 0 ]]; then
+#      echo "CouchDB did not run within $TIMEOUT seconds."
+#      exit 2
+#    fi
+#  done
+#}
+#
+#waiting
